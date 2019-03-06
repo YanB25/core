8a1621c406726064053344b79c4130ec8c8f3680
YanB25 <yanb25@mail2.sysu.edu.cn>
2019-03-06 16:53:21 +0800

    modify ganache-*.sh again to set --datadir - add {(main)|(side)}-ethereum/ dir - gitignore it.

--- blockchain/source/ganache-main.sh
+++ blockchain/source/ganache-main.sh
@@ -1,5 +1,7 @@
 ./node_modules/ganache-cli/cli.js \
     --accounts=20 --defaultBalanceEther=100000000 \
     --port=8545 --gasLimit=0xffffff --networkId=666 \
-    --deterministic --mnemonic="ganache-main" | tee ganache-main.log
+    --deterministic --mnemonic="ganache-main" \
+    --db="./main-ethereum" \
+    | tee ganache-main.log
     # --debug
--- blockchain/source/ganache-side.sh
+++ blockchain/source/ganache-side.sh
@@ -1,5 +1,7 @@
 ./node_modules/ganache-cli/cli.js \
     --accounts=20 --defaultBalanceEther=100000000 \
     --port=7545 --gasLimit=0xffffff --networkId=777 \
-    --deterministic --mnemonic="ganache-side" | tee ganache-side.log
+    --deterministic --mnemonic="ganache-side" \
+    --db="./side-ethereum" \
+    | tee ganache-side.log
     # --debug
