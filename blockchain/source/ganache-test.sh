73dbf3ffb2c25e89eac88277b20191dc4d83f7ce
yanbin <yanb25@mail2.sysu.edu.cn>
2019-03-08 17:55:19 +0800

    perfect-ize SNM.sol and add ganache-test.sh - contracts/SNM.sol:     - indent 4-space     - explicitly public visibility     - change deprecated throw to require and add proper error msg - ganache-test.sh: add short-cut cmd to run a test-net

---
+++ blockchain/source/ganache-test.sh
@@ -0,0 +1,4 @@
+./node_modules/ganache-cli/cli.js \
+    --accounts=20 --defaultBalanceEther=100000000 \
+    --port=9545 --gasLimit=0xffffff --networkId=9372 \
+    --deterministic --mnemonic="just a test"
