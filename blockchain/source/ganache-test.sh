d626815c64f961a6d97e28f982a3cb3df0b14e1f
yanbin <yanb25@mail2.sysu.edu.cn>
2019-03-11 11:05:35 +0800

    low down gas price and add up gas limit - ganache-test.sh: see above

--- blockchain/source/ganache-test.sh
+++ blockchain/source/ganache-test.sh
@@ -1,4 +1,5 @@
 ./node_modules/ganache-cli/cli.js \
     --accounts=20 --defaultBalanceEther=100000000 \
-    --port=9545 --gasLimit=0xffffff --networkId=9372 \
+    --gasPrice=100 --gasLimit=0xfffffffffffffff \
+    --port=9545  --networkId=9372 \
     --deterministic --mnemonic="just a test"
