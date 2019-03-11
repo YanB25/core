55ca1a2dfa776549548bc62281cc05193f1ce3b7
yanb25 <yanb25@mail2.sysu.edu.cn>
2019-03-11 16:37:47 +0800

    correctly config ganache-{main}|{side}.sh

--- blockchain/source/ganache-side.sh
+++ blockchain/source/ganache-side.sh
@@ -1,6 +1,7 @@
 ./node_modules/ganache-cli/cli.js \
     --accounts=20 --defaultBalanceEther=100000000 \
-    --port=7545 --gasLimit=0xffffff --networkId=777 \
+    --port=7545  --networkId=777 \
+    --gasLimit=0xfffffffffff --gasPrice=0x01 \
     --deterministic --mnemonic="ganache-side" \
     --db="./side-ethereum" \
     | tee ganache-side.log
